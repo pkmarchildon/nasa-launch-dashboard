061f48056343e45513addd3fff8323b329199441
Pierre-Karl Marchildon <pikou@Pierre-Karls-MacBook-Pro.local>
2022-11-29 19:45:22 -0500

    Mongo URL is stored as an environment variable.

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose');
 
-const MONGO_URL =
-  'mongodb+srv://pk:bonbon12@cluster0.sguocfw.mongodb.net/?retryWrites=true&w=majority';
-
 mongoose.connection.once('open', () => {
   console.log('MongoDB connection ready.');
 });
@@ -12,7 +9,7 @@ mongoose.connection.on('error', (err) => {
 });
 
 async function mongoConnect() {
-  await mongoose.connect(MONGO_URL, {
+  await mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   });
